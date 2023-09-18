91dbd3f88df293b450b44fef52b011c1840fd3df
Chip Castle <chip@chipcastle.com>
2023-09-18 14:09:41 -0500

    Add VueSelectQuantity component

---
+++ src/components/VueSelectQuantity.vue
@@ -0,0 +1,18 @@
+<template>
+  <select @change="$emit('vue-select-quantity:change', $event.target.value)">
+    <option value="0">0 (zzooo)</option>
+    <option value="0">0 (foobar)</option>
+    <option value="0">0 (Delete)</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="99">9</option>
+    <option value="10+">10+</option>
+  </select>
+</template>
