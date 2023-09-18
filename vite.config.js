434859b226e580b34276084541e6887020d8cf9c
Chip Castle <chip@chipcastle.com>
2023-09-18 14:11:17 -0500

    Add build config for distributing component library

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,25 @@
 const { createVuePlugin } = require('vite-plugin-vue2');
+const path = require('path');
+const { defineConfig } = require('vite');
 
-module.exports = {
-  plugins: [createVuePlugin()],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.js'),
+      name: 'VueSelectQuantity',
+      fileName: (format) => `vue-select-quantity.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
+  plugins: [createVuePlugin()]
+});
