e64ffd73720665de95851bb38732cb4a5bf6cd16
Chip Castle <chip@chipcastle.com>
2023-09-18 23:11:48 -0500

    Update text description of component in Demo.vue

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,29 +1,15 @@
 <template>
   <div>
 
-    <h1>{{ msg }}</h1>
+    <h1>VueSelectQuantity - component for vue 2</h1>
 
-    <p>
-      <a
-        href="https://vitejs.dev/guide/features.html"
-        target="_blank"
-      >Vite Documentation</a> |
-      <a
-        href="https://vuejs.org/v2/guide/"
-        target="_blank"
-      >Vue 2 Documentation</a>
+    <p align="center">
+      Edit
+      <code>components/Demo.vue</code> or
+      <code>components/VueSelectQuantity.vue</code>
+      to test hot module replacement
     </p>
 
-    <p>
-      Edit these components to test hot module replacement:
-
-      <ul>
-        <li><code>components/Demo.vue</code></li>
-        <li><code>components/VueSelectQuantity.vue</code></li>
-      </ul>
-    </p>
-
-    <h2>VueSelectQuantity component:</h2>
     <VueSelectQuantity
       @vue-select-quantity:change="() => change('change detected')"
     />
