f18c14b7c5b0d24bb8d86a4b45a3e412ca185523
Chip Castle <chip@chipcastle.com>
2023-09-19 19:40:04 -0500

    Add v-for to generate option values in SelectQuantity.vue

--- src/components/SelectQuantity.vue
+++ src/components/SelectQuantity.vue
@@ -9,17 +9,13 @@
       class="custom-select"
     >
       <option disabled value="">Please select quantity</option>
-      <option value="0">0 (Delete)</option>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10+</option>
+      <option v-for="(n, index) in Array(11).keys()"
+        :key="index"
+        :data-qty="n"
+      >
+        <span v-if="n === 0">{{n}} (Delete)</span>
+        <span v-else>{{ n !== 10 ? n : `${n}+` }}</span>
+      </option>
     </select>
   </div>
 </template>
