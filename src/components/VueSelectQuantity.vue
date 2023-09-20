95c55730948b743f39814fde3eaa4789c28c761c
Chip Castle <chip@chipcastle.com>
2023-09-19 23:40:33 -0500

    Update console.log in change fn for VueQuantitySelect.vue

--- src/components/VueSelectQuantity.vue
+++ src/components/VueSelectQuantity.vue
@@ -28,7 +28,7 @@ export default {
   },
   methods: {
     change (value) {
-      console.log('change value', value, 'change quantity', this.quantity)
+      console.log('VueSelectQuantity.vue change value', value, 'change quantity', this.quantity)
       if (value === "0") {
         console.log('this.$emit("vue-select-quantity:delete")')
         this.$emit("vue-select-quantity:delete")
