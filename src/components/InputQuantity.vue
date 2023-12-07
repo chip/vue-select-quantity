e46874596e10850157dc48a4ea3abcd48357ab7f
Chip Castle <chip@chipcastle.com>
2023-12-07 15:54:55 -0600

    Remove css from InputQuantity

--- src/components/InputQuantity.vue
+++ src/components/InputQuantity.vue
@@ -16,57 +16,10 @@ export default {
   props: {
     value: { type: Number, default: 0 }
   },
-  created () {
-    if (this.value) {
-      this.quantity = this.value
-    }
-  },
   data () {
     return {
-      quantity: 0
-    }
-  },
-  methods: {
-    cancel () {
-      console.log('cancel')
-      this.$emit("vue-select-quantity:cancel")
-    },
-    update () {
-      console.log('update', this.quantity)
-      this.$emit("vue-select-quantity:update", this.quantity)
+      quantity: this.value
     }
   }
 }
 </script>
-<style scoped>
-.vsc-text-input {
-  border: 0.12em solid rgb(208, 211, 211);
-  border-radius: 6px;
-  transition: 200ms box-shadow ease-in-out;
-  padding-left: 8px;
-}
-
-.vsc-text-input:focus {
-  outline: 0.36em solid rgb(197, 240, 248);
-  border: 0.12em solid rgb(25, 100, 120);
-}
-
-.vsc-button {
-  border: 1px solid rgb(245, 196, 48);
-  background-color: rgb(255, 211, 55);
-  border-radius: 6px;
-  -moz-border-radius: 6px;
-  -webkit-border-radius: 6px;
-  font-weight: 300;
-  padding: 6px 4px;
-  margin-left: 6px;
-}
-
-.vsc-button:hover {
-  background-color: rgb(245, 196, 48);
-}
-
-.vsc-cancel {
-  padding-left: 8px; 
-}
-</style>
