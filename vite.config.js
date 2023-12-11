f851bc149a9c329489a58b11a45165bdf243cc53
Chip Castle <chip@chipcastle.com>
2023-12-11 13:49:52 -0600

    Add working 'vite run build'; creates style.css, style.min.css under dist folder

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ const { createVuePlugin } = require('vite-plugin-vue2');
 const path = require('path');
 const { defineConfig } = require('vite');
 import { transform } from 'lightningcss';
-// import scss from 'rollup-plugin-scss';
 import copy from 'rollup-plugin-copy';
-import { createTransform } from 'rollup-copy-transform-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -25,48 +23,25 @@ export default defineConfig({
   },
   plugins: [
     copy({
-      verbose: true,
+      hook: "writeBundle",
       targets: [
-        // {
-        //   src: 'public/style.css',
-        //   dest: 'dist',
-        //   rename: 'style.min.css',
-        //   // transform: createTransform({ inline: false, minify: true })
-        //   transform: function(content, filename) {
-        //     console.log('transform', content, filename)
-        //     // let { code, map } = transform({
-        //     //   // ...
-        //     //   minify: true
-        //     // });
-        //     // console.log('code', code, 'map', map)
-        //     return content
-        //   }
-        // },
         {
           src: 'public/style.css',
-          dest: 'public',
-          // rename: 'style.min.css',
-          rename: (name, extension, fullPath) => {
-            console.log('rename()', 'name', name, 'extension', extension, 'fullPath', fullPath)
-            return `${name}.min.${extension}`
-          },
-          // transform: createTransform({ inline: true, minify: true, map: { inline: true }}),
-          transform: (source, id) => {
-            console.log('transform()', 'source', source, 'typeof', typeof(source), 'id', id) 
+          dest: 'dist',
+          rename: 'style.css'
+        },
+        {
+          src: 'public/style.css',
+          dest: 'dist',
+          rename: 'style.min.css',
+          transform: (source) => {
             let { code } = transform({
-              filename: 'public/style.min.css',
+              filename: 'style.css',
               minify: true,
               code: source
             })
             return code.toString()
-          },
-          // copySync: true
-        },
-        {
-          src: 'public/style.css',
-          dest: 'tmp',
-          rename: 'style.css',
-          // copySync: true
+          }
         }
       ]
     }),
