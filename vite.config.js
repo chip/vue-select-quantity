13a9037a0b23837f566eaf80eaa1dd25b9d3cb67
Chip Castle <chip@chipcastle.com>
2023-12-11 12:57:01 -0600

    Add working css minification, but files are created under public dir

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 const { createVuePlugin } = require('vite-plugin-vue2');
 const path = require('path');
 const { defineConfig } = require('vite');
-import scss from 'rollup-plugin-scss';
+import { transform } from 'lightningcss';
+// import scss from 'rollup-plugin-scss';
 import copy from 'rollup-plugin-copy';
+import { createTransform } from 'rollup-copy-transform-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,18 +24,49 @@ export default defineConfig({
     }
   },
   plugins: [
-    scss({
-      name: 'vue-select-quantity.min.css',
-      sourceMap: false,
-      outputStyle: 'compressed'
-    }),
     copy({
       verbose: true,
       targets: [
+        // {
+        //   src: 'public/style.css',
+        //   dest: 'dist',
+        //   rename: 'style.min.css',
+        //   // transform: createTransform({ inline: false, minify: true })
+        //   transform: function(content, filename) {
+        //     console.log('transform', content, filename)
+        //     // let { code, map } = transform({
+        //     //   // ...
+        //     //   minify: true
+        //     // });
+        //     // console.log('code', code, 'map', map)
+        //     return content
+        //   }
+        // },
         {
-          src: 'public/vue-select-quantity.css',
-          dest: 'dist',
-          rename: 'vue-select-quantity.css',
+          src: 'public/style.css',
+          dest: 'public',
+          // rename: 'style.min.css',
+          rename: (name, extension, fullPath) => {
+            console.log('rename()', 'name', name, 'extension', extension, 'fullPath', fullPath)
+            return `${name}.min.${extension}`
+          },
+          // transform: createTransform({ inline: true, minify: true, map: { inline: true }}),
+          transform: (source, id) => {
+            console.log('transform()', 'source', source, 'typeof', typeof(source), 'id', id) 
+            let { code } = transform({
+              filename: 'public/style.min.css',
+              minify: true,
+              code: source
+            })
+            return code.toString()
+          },
+          // copySync: true
+        },
+        {
+          src: 'public/style.css',
+          dest: 'tmp',
+          rename: 'style.css',
+          // copySync: true
         }
       ]
     }),
